f101bbe84fd521d5438e4ccf28dc483a349dbde8
Samantha Kammerer <137442626+samarz03@users.noreply.github.com>
2026-08-31 19:35:22 -0400

    Add files via upload

---
+++ HW1/Hw1/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Hw1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lavender;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(197, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BINARY";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MintCream;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DECIMAL INPUT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lavender;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(310, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "HEXADECIMAL";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MintCream;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(544, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OUTPUT";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MintCream;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(544, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "OUTPUT";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lavender;
+            this.button3.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(313, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 41);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "HEXADECIMAL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MintCream;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "BINARY INPUT";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Lavender;
+            this.button4.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(197, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "DECIMAL";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.MintCream;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(544, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "OUTPUT";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Lavender;
+            this.button5.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(368, 245);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 41);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "DECIMAL";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.MintCream;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(225, 21);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "HEXADECIMAL INPUT";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Lavender;
+            this.button6.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(264, 245);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 41);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "BINARY";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Thistle;
+            this.textBox1.Location = new System.Drawing.Point(16, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Thistle;
+            this.textBox2.Location = new System.Drawing.Point(516, 62);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Thistle;
+            this.textBox3.Location = new System.Drawing.Point(16, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Thistle;
+            this.textBox4.Location = new System.Drawing.Point(516, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Thistle;
+            this.textBox5.Location = new System.Drawing.Point(16, 259);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(149, 20);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.Thistle;
+            this.textBox6.Location = new System.Drawing.Point(516, 259);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(149, 20);
+            this.textBox6.TabIndex = 17;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.CadetBlue;
+            this.button7.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.Location = new System.Drawing.Point(12, 311);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(147, 43);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "RESET";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(702, 363);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BASE CONVERTER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ HW1/Hw1/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Hw1
+{
+    public partial class Form1 : Form
+    {
+        int inp = 0; //Initialize input int
+        int[] cat= new int[8]; //Initialize array for binary
+        char[] hex = new char[2]; //Initialize arry for Hexadecimal
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Decimal to Binary
+        {
+            inp = int.Parse(textBox1.Text); //get data from textbox
+            for (int i=0; i<=7; i++) //Initialize for loop
+            {
+                cat[7-i] = inp % 2; //put remainder in array
+                inp = inp / 2;      //update input
+            }
+            textBox2.Text = cat[0].ToString() + cat[1].ToString() + cat[2].ToString() + cat[3].ToString() +
+                cat[4].ToString() + cat[5].ToString() + cat[6].ToString() + cat[7].ToString(); //output data to textbox
+        }
+        private void button2_Click(object sender, EventArgs e) //Decimal to Hexadecimal
+        {
+            inp = int.Parse(textBox1.Text); //get data from textbox
+            for (int i = 0; i <= 1; i++) //Initialize for loop
+            {
+                int hexhold = inp % 16; //Get remainder for coniverision
+                if (hexhold == 10) //Check if reminder needs to be changed into letter
+                {
+                    hex[1-i] = 'A'; //Change remainder to A
+                }
+                if (hexhold == 11)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'B'; //Change remainder to B
+                }
+                if (hexhold == 12)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'C'; //Change remainder to C
+                }
+                if (hexhold == 13)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'D'; //Change remainder to D
+                }
+                if (hexhold == 14)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'E'; //Change remainder to E
+                }
+                if (hexhold == 15)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'F'; //Change remainder to F
+                }
+
+                if (hexhold < 10) //Raminder doesnt need to be changed to letter
+                {
+                    hexhold = hexhold + 48; //Change to character for array
+                    hex[1 - i] = (char)hexhold; //Input into array
+                }
+                inp = inp / 16;        //update input
+            }
+            textBox2.Text = hex[0].ToString() + hex[1].ToString(); //output data to textbox
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Binary to Decimal
+        {
+            double inl = 0; //create double var
+            char[] inpArr = textBox3.Text.ToArray<char>(); //parse into array
+
+            for (int i = 0; i <= 7; i++) //Initialize for loop
+            {
+                double binhold = inpArr[7 - i] - '0'; //change array value into double
+                binhold = binhold * Math.Pow(2.0, i); //Multiply value by power of 2
+                inl = binhold + inl; //update output
+            }
+            textBox4.Text = inl.ToString(); //output data to textbox
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Binary to Hexadecimal
+        {
+            double inl = 0; //create double var
+            char[] inpArr = textBox3.Text.ToArray<char>(); //parse into array
+
+            for (int i = 0; i <= 7; i++) //Initialize for loop to change into decimal
+            {
+                double binhold = inpArr[7 - i] - '0'; //change array value into double
+                binhold = binhold * Math.Pow(2.0, i); //Multiply value by power of 2
+                inl = binhold + inl; //update output
+            }
+            int inp = (int) inl; //change double to integer
+
+            for (int i = 0; i <= 1; i++) //Initialize for loop to change into hexadecimal
+            {
+                int hexhold = inp % 16; //Get remainder for conversion
+                if (hexhold == 10) //Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'A'; //Change remainder to A
+                }
+                if (hexhold == 11)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'B'; //Change remainder to B
+                }
+                if (hexhold == 12)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'C'; //Change remainder to C
+                }
+                if (hexhold == 13)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'D'; //Change remainder to D
+                }
+                if (hexhold == 14)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'E'; //Change remainder to E
+                }
+                if (hexhold == 15)//Check if reminder needs to be changed into letter
+                {
+                    hex[1 - i] = 'F'; //Change remainder to F
+                }
+
+                if (hexhold < 10) //Raminder doesnt need to be changed to letter
+                {
+                    hexhold = hexhold + 48; //Change to character for array
+                    hex[1 - i] = (char)hexhold; //Input into array
+                }
+                inp = inp / 16;        //update input
+            }
+            textBox4.Text = hex[0].ToString() + hex[1].ToString(); //output data to textbox
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) //Hexadecimal to Binary
+        {
+            double inl = 0; //create double var
+            char[] inpArr = textBox5.Text.ToArray<char>(); //parse into array
+            double binhold = 0; //Initialize binhold varible
+            for (int i = 0; i <= 1; i++) //Initialize for loop
+            {
+                if (inpArr[1 - i] == 'A') //Check if value is A
+                {
+                    binhold = 10; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'B') //Check if value is B
+                {
+                    binhold = 11; //Change output to quavuilanet number
+                }
+                else if (inpArr[1 - i] == 'C') //Check if value is C
+                {
+                    binhold = 12; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'D') //Check if value is D
+                {
+                    binhold = 13; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'E') //Check if value is E
+                {
+                    binhold = 14; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'F') //Check if value is F
+                {
+                    binhold = 15; //Change output to equaivilent number
+                }
+                else //If NOT a letter
+                {
+                    binhold = inpArr[1 - i] - '0'; //change array value into double
+                }
+
+                binhold = binhold * Math.Pow(16, i); //Multiply value by power of 16
+                inl = binhold + inl; //update output
+            }
+            int binar = (int) inl;
+            for (int i = 0; i <= 7; i++) //Initialize for loop to change into binary
+            {
+                cat[7 - i] = binar % 2; //put remainder in array
+                binar = binar / 2;      //update input
+            }
+            textBox6.Text = cat[0].ToString() + cat[1].ToString() + cat[2].ToString() + cat[3].ToString() +
+                cat[4].ToString() + cat[5].ToString() + cat[6].ToString() + cat[7].ToString(); //output data to textbox
+        }
+        private void button5_Click(object sender, EventArgs e) //Hexadecimal to Decimal
+        {
+            double inl = 0; //create double var
+            char[] inpArr = textBox5.Text.ToArray<char>(); //parse into array
+            double binhold = 0; //Initialize binhold varible
+            for (int i = 0; i <= 1; i++) //Initialize for loop
+            {
+                if (inpArr[1 - i] == 'A') //Check if value is A
+                {
+                    binhold = 10; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'B') //Check if value is B
+                {
+                    binhold = 11; //Change output to quavuilanet number
+                }
+                else if (inpArr[1 - i] == 'C') //Check if value is C
+                {
+                    binhold = 12; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'D') //Check if value is D
+                {
+                    binhold = 13; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'E') //Check if value is E
+                {
+                    binhold = 14; //Change output to equaivilent number
+                }
+                else if (inpArr[1 - i] == 'F') //Check if value is F
+                {
+                    binhold = 15; //Change output to equaivilent number
+                }
+                else //If NOT a letter
+                {
+                    binhold = inpArr[1 - i] - '0'; //change array value into double
+                }
+
+                binhold = binhold * Math.Pow(16, i); //Multiply value by power of 16
+                inl = binhold + inl; //update output
+            }
+            textBox6.Text = inl.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+    }
+}
